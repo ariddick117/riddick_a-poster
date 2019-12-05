aa8b421842aeb915caa6d95886a96d1250edf92d
Alec <a_riddick@fanshaweonline.ca>
2019-12-05 16:52:16 -0500

    Almost Working Popups

--- public/js/main.js
+++ public/js/main.js
@@ -1,58 +1,38 @@
 (() => {
 
     // get the span tags to be able to click them
-    let seeMoreButtons = document.querySelectorAll('.see-more'),
-        popOver = document.querySelector('.popover');
+    let buttonStrong = document.querySelector('.strength'),
+        buttonWeak = document.querySelector('.weak'),
 
-    // set up some waypoints and make things happen
-    var waypoint = new Waypoint({
-        element: document.querySelector('#beer1'),
-        handler: function(direction) {
-          console.log('Scrolled to waypoint!');
-
-          // drill down into the SVG from the HTML doc / JS file
-          // changes things top-level, instead of inside the SVG
-          let svg = this.element.firstElementChild.contentDocument;
-          // debugger;
-
-
-          // NOT WORKING YET BUT YOU GET THE IDEA
-          // the yellow class is in main.css
-          svg.querySelector("#beerText").classList.add("yellow");
-        },
-    });
-
-    let waypoint2 = new Waypoint({
-        element: document.querySelector('#beer2'),
-        handler: function(direction) {
-            console.log('Scrolled to waypoint!')
-            this.element.innerHTML += "<p> I got added with Waypoint!</p>"
-        },
-
-        offset: 300 // waypoint needs to hit the top of the viewport, if that's not possible, use an offset
-    });
+        popOverStrong = document.querySelector('.popover-s');
+        popOverWeak = document.querySelector('.popover-w');
 
     const theSVGwrapper = document.querySelector('.svg-wrapper');
 
-    // function showSVG(event) {
-    //     //debugger;
-    // }
+    //show the popover    
+    function showPopoverStrong(beerdata, el) {
+        popOverStrong.querySelector(".ipa-rating").textContent = `IPA Rating: ${beerdata.IpaRating}`;
+        popOverStrong.querySelector(".ratings").textContent = `IPA Rating: ${beerdata.IpaRating}`;
+        popOverStrong.querySelector(".beer-description").textContent = beerdata.description;
 
-    // theSVGwrapper.addEventListener('mouseover', showSVG);
+        popOverStrong.classList.toggle('show-popover-s');
 
-    //show the popover
-    function showPopover(beerdata, el) {
-        popOver.querySelector(".ipa-rating").textContent = `IPA Rating: ${beerdata.IpaRating}`;
-        popOver.querySelector(".ratings").textContent = `IPA Rating: ${beerdata.IpaRating}`;
-        popOver.querySelector(".beer-description").textContent = beerdata.description;
-
-        popOver.classList.add('show-popover');
-
-        el.appendChild(popOver);
+        el.appendChild(popOverStrong);
     }
 
+    function showPopoverWeak(beerdata, el) {
+        popOverWeak.querySelector(".ipa-rating").textContent = `IPA Rating: ${beerdata.IpaRating}`;
+        popOverWeak.querySelector(".ratings").textContent = `IPA Rating: ${beerdata.IpaRating}`;
+        popOverWeak.querySelector(".beer-description").textContent = beerdata.description;
+
+        popOverWeak.classList.toggle('show-popover-w');
+
+        el.appendChild(popOverWeak);
+    }
+
+
     // do fetch call to database
-    function fetchData() {
+    function fetchDataS() {
         let url = `/info/${this.dataset.target}`,
             targetElement = this;
 
@@ -60,12 +40,28 @@
         .then(res => res.json())
         .then(data => {
             console.log(data)
-            showPopover(data, targetElement);
+            showPopoverStrong(data, targetElement);
         })
         .catch(function(error) {
             console.log(error);
         })
     }
 
-    seeMoreButtons.forEach(button => button.addEventListener('click', fetchData));
+    function fetchDataW() {
+        let url = `/info/${this.dataset.target}`,
+            targetElement = this;
+
+        fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            showPopoverWeak(data, targetElement);
+        })
+        .catch(function(error) {
+            console.log(error);
+        })
+    }
+
+    buttonStrong.addEventListener('click', fetchDataS);
+    buttonWeak.addEventListener('click', fetchDataW);
 })();
