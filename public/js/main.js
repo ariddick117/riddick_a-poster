200a6de370631365cb10db751a0f121159812ed9
Alec <a_riddick@fanshaweonline.ca>
2019-12-05 23:07:07 -0500

    Database Linked

--- public/js/main.js
+++ public/js/main.js
@@ -7,23 +7,37 @@
         popOverStrong = document.querySelector('.popover-s');
         popOverWeak = document.querySelector('.popover-w');
 
-    const theSVGwrapper = document.querySelector('.svg-wrapper');
+    //const theSVGwrapper = document.querySelector('.svg-wrapper');
 
     //show the popover    
     function showPopoverStrong(beerdata, el) {
-        popOverStrong.querySelector(".ipa-rating").textContent = `IPA Rating: ${beerdata.IpaRating}`;
-        popOverStrong.querySelector(".ratings").textContent = `IPA Rating: ${beerdata.IpaRating}`;
-        popOverStrong.querySelector(".beer-description").textContent = beerdata.description;
+        popOverStrong.querySelector(".strong1").textContent = beerdata.strong1;
+        popOverStrong.querySelector(".strong2").textContent = beerdata.strong2;
+        popOverStrong.querySelector(".strong3").textContent = beerdata.strong3;
+        popOverStrong.querySelector(".strong4").textContent = beerdata.strong4;
+        popOverStrong.querySelector(".strong5").textContent = beerdata.strong5;
+        popOverStrong.querySelector(".strong6").textContent = beerdata.strong6;
+        popOverStrong.querySelector(".strong7").textContent = beerdata.strong7;
+        popOverStrong.querySelector(".strong8").textContent = beerdata.strong8;
+        popOverStrong.querySelector(".strong9").textContent = beerdata.strong9;
+        // popOverStrong.querySelector(".strong10").textContent = beerdata.strong10;
+        // popOverStrong.querySelector(".strong11").textContent = beerdata.strong11;
+        // popOverStrong.querySelector(".strong12").textContent = beerdata.strong12;
+        // popOverStrong.querySelector(".strong13").textContent = beerdata.strong13;
+        // popOverStrong.querySelector(".strong14").textContent = beerdata.strong14;
+        // popOverStrong.querySelector(".strong15").textContent = beerdata.strong15;
+        // popOverStrong.querySelector(".strong16").textContent = beerdata.strong16;
+        // popOverStrong.querySelector(".strong17").textContent = beerdata.strong17;
 
         popOverStrong.classList.toggle('show-popover-s');
 
         el.appendChild(popOverStrong);
     }
 
-    function showPopoverWeak(beerdata, el) {
-        popOverWeak.querySelector(".ipa-rating").textContent = `IPA Rating: ${beerdata.IpaRating}`;
-        popOverWeak.querySelector(".ratings").textContent = `IPA Rating: ${beerdata.IpaRating}`;
-        popOverWeak.querySelector(".beer-description").textContent = beerdata.description;
+    function showPopoverWeak(weakness, el) {
+        popOverWeak.querySelector(".weak1").textContent = weakness.strong1;
+        popOverWeak.querySelector(".weak2").textContent = weakness.strong2;
+        popOverWeak.querySelector(".weak3").textContent = weakness.strong3;
 
         popOverWeak.classList.toggle('show-popover-w');
 
